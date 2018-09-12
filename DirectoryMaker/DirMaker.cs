afba90603976ec6f502f09c81ada011a27b7b9ef
Maxim.Kimaev <maxim.kimaev@gmail.com>
2018-09-12 15:41:05 +0300

    Add project files.

---
+++ DirectoryMaker/DirMaker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirectoryMaker
+{
+    class DirMaker
+    {
+        public string Result { get; set; }
+        public DirMaker() { }
+        public void CreateDirectory(string dirName)
+        {
+            string[] data3 = TKD.dataTkd.Split('*');
+            //dirName = "MDU_KHA00078";
+            //string pathDir = @"D:\MDU_Kharkiv_cluster";
+            string pathDir = Environment.CurrentDirectory;
+            DirectoryInfo dirInfo = new DirectoryInfo(pathDir);
+            var item = from t in data3
+                       where t.Contains(dirName)
+                       //select t.TrimStart(new char {'\','r\n' });
+                       select t;
+            foreach (string mytkd in item)
+            {
+                string mytkd2 = mytkd.Substring(2);
+                string subpath = dirName + "\\" + mytkd2;
+
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+
+                try
+                {
+                    dirInfo.CreateSubdirectory(subpath);
+                    Result = String.Format("Папки по {0} созданы. Путь {1}", dirName, pathDir);
+                }
+                catch (Exception e)
+                {
+                    Result = e.Message;
+                }
+            }
+            
+        }
+    }
+}
---
+++ DirectoryMaker/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 12)
+            {
+                if (textBox1.Text.StartsWith("MDU_"))
+                {
+                    DirMaker maker1 = new DirMaker();
+                    maker1.CreateDirectory(textBox1.Text);
+                    MessageBox.Show(maker1.Result);
+                }
+                else
+                {
+                    info_label2.Visible = true;
+                    info_label2.ForeColor = Color.Red;
+                    info_label2.Text = @"Вы ввели неправильное название. 
+Пример: MDU_KHA00078";
+                }
+            }
+            if (textBox1.Text.Length < 12)
+            {
+                info_label2.Visible = true;
+                info_label2.ForeColor = Color.OrangeRed;
+                info_label2.Text = @"Вы ввели недостаточно символов!
+                    не менее 12";
+            }
+        }
+    }
+}
